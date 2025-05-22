a29d05603664ac9f679521846a4a4ecc7209c44a
uneek03 <uneek03@gmail.com>
2025-05-21 19:49:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 10000;
 
-// Serve static files
 app.use(express.static(path.join(__dirname)));
 
-// Fallback route
-app.get('*', (req, res) => {
+// Only respond to root URL
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
